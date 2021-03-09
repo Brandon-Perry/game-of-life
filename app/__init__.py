84d91c9bf2cd2e06bac8ba0e19fea0e4216ea21c
Brandon-Perry <brandonperryofficial@gmail.com>
2021-03-09 18:47:32 -0500

    changed around the way db was created to prevent circular imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy 
 
+from config import Config
+
+#Initialize application
 app = Flask(__name__)
+app.config.from_object(Config)
+
+#Set up db
+db = SQLAlchemy(app)
+
+
+migrate = Migrate(app, db)
+
 
 
 @app.route('/')
