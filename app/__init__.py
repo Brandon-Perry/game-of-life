a3dd2197f2bfb9fd7f13692e6461af20a94dacf7
Brandon-Perry <brandonperryofficial@gmail.com>
2021-03-09 19:53:22 -0500

    added flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy 
+from flask_login import LoginManager
 
 from config import Config
+from .models import db
 
 #Initialize application
 app = Flask(__name__)
+
+login = LoginManager(app)
+login.login_view = 'auth.unauthorized'
+
 app.config.from_object(Config)
 
 #Set up db
-db = SQLAlchemy(app)
-
-
-migrate = Migrate(app, db)
+db.init_app(app)
+Migrate(app, db)
 
 
 
